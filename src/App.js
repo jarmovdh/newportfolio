123afbe7b54fa26d8d725c6ee429b7048c7d205a
qwertmedia <jarmovdh@gmail.com>
2022-03-13 13:42:15 +0100

    framer motion setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from "react";
 import "./App.scss";
 import { Routes, Route } from "react-router-dom";
+import { CSSTransition } from "react-transition-group";
+import { gsap } from "gsap";
 
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Header from "./components/Header";
 
-const routes = [
-  { path: "/", name: "Home", Component: Home },
-  { path: "/About", name: "About", Component: About },
-];
-
 function App() {
+  const onEnter = (node) => {
+    console.log(node);
+  };
+
+  const onExit = (node) => {
+    console.log(node);
+  };
+
   return (
     <>
       <Header />
       <div className="container">
         <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/About" element={<About />} />
+          <Route exact path="/" element={<Home />}></Route>
+          <Route path="/About" element={<About />}></Route>
         </Routes>
       </div>
     </>
