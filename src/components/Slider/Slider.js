b080f94ce54c9b9f81674272b6ac535f0ed7ad27
qwertmedia <[jarmovdh@gmail.com]>
2022-03-30 14:36:55 +0200

    gsap slider

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,10 +1,10 @@
-import React, { useEffect, useRef } from "react";
-import { gsap } from "gsap";
+import React, { useEffect, useRef, useState } from "react";
+import { gsap, Power3 } from "gsap";
 import "./Slider.scss";
 
 import leftArrow from "./assets/previous.svg";
 import rightArrow from "./assets/next.svg";
-import image1 from "./assets/adidas1.jpg";
+
 const content = [
   {
     title: "Adidas.nl",
@@ -30,10 +30,39 @@ const Slider = () => {
   let imageList = useRef(null);
   let textList = useRef(null);
   let categoryList = useRef(null);
+  const [state, setState] = useState({
+    isActive1: true,
+    isActive2: false,
+    isActive3: false,
+  });
+
+  const imageWidth = 500;
 
   useEffect(() => {
-    console.log(imageList.children[0]);
+    gsap.to(categoryList.children[0], { duration: 0, opacity: 1 });
+    gsap.to(textList.children[0], { duration: 0, opacity: 1 });
   }, []);
+
+  const slideLeft = (index, duration, multiplied = 1) => {
+    gsap.to(imageList.children[index], {
+      duration: 1,
+      x: -imageWidth * multiplied,
+      ease: Power3.easeIn,
+    });
+  };
+
+  const nextSlide = () => {
+    if (imageList.children[0].classList.contains("active")) {
+      setState({ isActive1: false, isActive2: true });
+
+      slideLeft(0, 1);
+      slideLeft(1, 1);
+    } else if (imageList.children[1].classList.contains("active")) {
+      setState({ isActive2: false, isActive3: true });
+    } else if (imageList.children[2].classList.contains("active")) {
+      setState({ isActive1: true, isActive3: false });
+    }
+  };
 
   return (
     <div className="home-slider-section">
@@ -41,19 +70,19 @@ const Slider = () => {
         <div className="column-left">
           <div className="slider-content-left">
             <ul ref={(el) => (textList = el)}>
-              <li>
+              <li className={state.isActive1 ? "active" : ""}>
                 <div className="content-home">
                   <h3 className="title-project">{content[0].title}</h3>
                   <p className="info">{content[0].info}</p>
                 </div>
               </li>
-              <li>
+              <li className={state.isActive2 ? "active" : ""}>
                 <div className="content-home">
                   <h3 className="title-project">{content[1].title}</h3>
                   <p className="info">{content[1].info}</p>
                 </div>
               </li>
-              <li>
+              <li className={state.isActive3 ? "active" : ""}>
                 <div className="content-home">
                   <h3 className="title-project">{content[2].title}</h3>
                   <p className="info">{content[2].info}</p>
@@ -66,7 +95,7 @@ const Slider = () => {
                   <img src={leftArrow} alt="left arrow" />
                 </span>
               </div>
-              <div className="arrow-right">
+              <div onClick={nextSlide} className="arrow-right">
                 <span>
                   <img src={rightArrow} alt="left arrow" />
                 </span>
@@ -75,17 +104,17 @@ const Slider = () => {
           </div>
           <div className="slider-content-right">
             <ul ref={(el) => (categoryList = el)}>
-              <li>
+              <li className={state.isActive1 ? "active" : ""}>
                 <div className="category-home">
                   <h3 className="category-project">{content[0].category}</h3>
                 </div>
               </li>
-              <li>
+              <li className={state.isActive2 ? "active" : ""}>
                 <div className="category-home">
                   <h3 className="category-project">{content[1].category}</h3>
                 </div>
               </li>
-              <li>
+              <li className={state.isActive3 ? "active" : ""}>
                 <div className="category-home">
                   <h3 className="category-project">{content[2].category}</h3>
                 </div>
@@ -96,21 +125,21 @@ const Slider = () => {
         <div className="column-right">
           <div className="slider-image">
             <ul ref={(el) => (imageList = el)}>
-              <li>
+              <li className={state.isActive1 ? "active" : ""}>
                 <img
                   src="./images/adidas1.jpg"
                   alt="{content[0].title}"
                   width="100%"
                 />{" "}
               </li>
-              <li>
+              <li className={state.isActive2 ? "active" : ""}>
                 <img
                   src="./images/products.jpg"
                   alt="{content[0].title}"
                   width="100%"
                 />
               </li>
-              <li>
+              <li className={state.isActive3 ? "active" : ""}>
                 <img
                   src="./images/shoot1.jpg"
                   alt="{content[0].title}"
